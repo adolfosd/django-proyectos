bc3cba09ef1aa9c231c78f9fe0777a0d5ad16f31
Adolfo Sánchez <adolfo.sanchez.dominguez@gmail.com>
2023-02-19 21:16:01 +0100

    pip install commented

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -o errexit
 
 #chmod a+x build.shpoetry install
-pip install -r requirements.txt
+#pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
